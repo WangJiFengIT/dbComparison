0ad8a93cd858cd32d1eaf518fb6510bb9ba3aa68
wangjifeng <wangjifeng@21cnjy.com>
2019-08-15 15:01:43 +0800

    添加项目文件。

---
+++ dbComparison/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace dbComparison
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_ip1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_name1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_pwd1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_ip2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_name2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_pwd2 = new System.Windows.Forms.TextBox();
+            this.btn_cn1 = new System.Windows.Forms.Button();
+            this.btn_cn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ip:";
+            // 
+            // txt_ip1
+            // 
+            this.txt_ip1.Location = new System.Drawing.Point(72, 41);
+            this.txt_ip1.Name = "txt_ip1";
+            this.txt_ip1.Size = new System.Drawing.Size(102, 21);
+            this.txt_ip1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "比对";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "name:";
+            // 
+            // txt_name1
+            // 
+            this.txt_name1.Location = new System.Drawing.Point(221, 41);
+            this.txt_name1.Name = "txt_name1";
+            this.txt_name1.Size = new System.Drawing.Size(102, 21);
+            this.txt_name1.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(329, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "password:";
+            // 
+            // txt_pwd1
+            // 
+            this.txt_pwd1.Location = new System.Drawing.Point(394, 41);
+            this.txt_pwd1.Name = "txt_pwd1";
+            this.txt_pwd1.Size = new System.Drawing.Size(102, 21);
+            this.txt_pwd1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ip:";
+            // 
+            // txt_ip2
+            // 
+            this.txt_ip2.Location = new System.Drawing.Point(72, 87);
+            this.txt_ip2.Name = "txt_ip2";
+            this.txt_ip2.Size = new System.Drawing.Size(102, 21);
+            this.txt_ip2.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(180, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(180, 90);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 12);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "name:";
+            // 
+            // txt_name2
+            // 
+            this.txt_name2.Location = new System.Drawing.Point(221, 87);
+            this.txt_name2.Name = "txt_name2";
+            this.txt_name2.Size = new System.Drawing.Size(102, 21);
+            this.txt_name2.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(329, 90);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 12);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "password:";
+            // 
+            // txt_pwd2
+            // 
+            this.txt_pwd2.Location = new System.Drawing.Point(394, 87);
+            this.txt_pwd2.Name = "txt_pwd2";
+            this.txt_pwd2.Size = new System.Drawing.Size(102, 21);
+            this.txt_pwd2.TabIndex = 2;
+            // 
+            // btn_cn1
+            // 
+            this.btn_cn1.Location = new System.Drawing.Point(515, 39);
+            this.btn_cn1.Name = "btn_cn1";
+            this.btn_cn1.Size = new System.Drawing.Size(75, 23);
+            this.btn_cn1.TabIndex = 3;
+            this.btn_cn1.Text = "连接";
+            this.btn_cn1.UseVisualStyleBackColor = true;
+            this.btn_cn1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btn_cn2
+            // 
+            this.btn_cn2.Location = new System.Drawing.Point(515, 85);
+            this.btn_cn2.Name = "btn_cn2";
+            this.btn_cn2.Size = new System.Drawing.Size(75, 23);
+            this.btn_cn2.TabIndex = 3;
+            this.btn_cn2.Text = "连接";
+            this.btn_cn2.UseVisualStyleBackColor = true;
+            this.btn_cn2.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 196);
+            this.Controls.Add(this.btn_cn2);
+            this.Controls.Add(this.btn_cn1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_pwd2);
+            this.Controls.Add(this.txt_pwd1);
+            this.Controls.Add(this.txt_name2);
+            this.Controls.Add(this.txt_name1);
+            this.Controls.Add(this.txt_ip2);
+            this.Controls.Add(this.txt_ip1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "mssql表结构比对器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_ip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_name1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_pwd1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_ip2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_name2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_pwd2;
+        private System.Windows.Forms.Button btn_cn1;
+        private System.Windows.Forms.Button btn_cn2;
+    }
+}
+
---
+++ dbComparison/SqlHelper.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbComparison
+{
+    public class SqlHelper
+    {
+        private static SqlConnection sqlConnection;
+
+        #region SQL
+        private static readonly string connectionString = "Data Source={0};Initial Catalog={1};User ID={2};Password={3}";
+        private static readonly string sqlGetDataBases = "select * from sys.databases";
+        private static readonly string sqlGetObjects = "select * from {0}.sys.objects";
+        private static readonly string sqlGetTables = sqlGetObjects + " where type='U'";
+        private static string sqlUseDataBase = string.Empty;//记录进入了哪个数据库
+        private static string sqlUesTable = string.Empty;//记录进入了哪个表
+        private static readonly string QUOTED_IDENTIFIER = "SET QUOTED_IDENTIFIER OFF";//让SQL启用双引号
+        private static readonly string sqlGetTableInfo = QUOTED_IDENTIFIER + @"
+                                                                                SELECT 
+                                                                                CASE WHEN col.colorder = 1 THEN obj.name ELSE '' END AS 表名,
+                                                                                col.colorder AS 序号,
+                                                                                col.name AS 列名,
+                                                                                ISNULL(ep.[value], '') AS 列说明,
+                                                                                t.name AS 数据类型,
+                                                                                col.length AS 长度,
+                                                                                ISNULL(COLUMNPROPERTY(col.id, col.name, 'Scale'), 0) AS 小数位数,
+                                                                                CASE WHEN COLUMNPROPERTY(col.id, col.name, 'IsIdentity') = 1 THEN '√' ELSE '' END AS 标识 ,
+                                                                                CASE WHEN EXISTS (SELECT 1
+	                                                                                FROM dbo.sysindexes si
+	                                                                                INNER JOIN dbo.sysindexkeys sik ON si.id = sik.id
+	                                                                                AND si.indid = sik.indid
+	                                                                                INNER JOIN dbo.syscolumns sc ON sc.id = sik.id
+	                                                                                AND sc.colid = sik.colid
+	                                                                                INNER JOIN dbo.sysobjects so ON so.name = si.name
+	                                                                                AND so.xtype = 'PK'
+	                                                                                WHERE sc.id = col.id
+	                                                                                AND sc.colid = col.colid) THEN '√' ELSE '' END AS 主键,
+                                                                                CASE WHEN col.isnullable = 1 THEN '√' ELSE '' END AS 允许空,
+                                                                                ISNULL(comm.text, '') AS 默认值
+                                                                                FROM dbo.syscolumns col
+                                                                                LEFT JOIN dbo.systypes t ON col.xtype = t.xusertype
+                                                                                INNER JOIN dbo.sysobjects obj ON col.id = obj.id
+                                                                                AND obj.xtype = 'U'
+                                                                                AND obj.status >= 0
+                                                                                LEFT JOIN dbo.syscomments comm ON col.cdefault = comm.id
+                                                                                LEFT JOIN sys.extended_properties ep ON col.id = ep.major_id
+                                                                                AND col.colid = ep.minor_id
+                                                                                AND ep.name = 'MS_Description'
+                                                                                LEFT JOIN sys.extended_properties epTwo ON obj.id = epTwo.major_id
+                                                                                AND epTwo.minor_id = 0
+                                                                                AND epTwo.name = 'MS_Description'
+                                                                                WHERE obj.name = " + "\"{0}\"" + @"
+                                                                                ORDER BY col.colorder;";
+        #endregion
+
+        private static object obj = new object();
+        public SqlHelper() { }
+        /// <summary>
+        /// 打开数据库
+        /// </summary>
+        /// <param name="DataSource">数据库地址</param>
+        /// <param name="UserId">用户名</param>
+        /// <param name="Paassword">密码</param>
+        /// <param name="DataBase">数据库名称【默认：master】</param>
+        public static void Open(string DataSource, string UserId, string Paassword, string DataBase = "master")
+        {
+            sqlConnection = new SqlConnection(string.Format(connectionString, DataSource, DataBase, UserId, Paassword));
+            sqlConnection.Open();
+        }
+        /// <summary>
+        /// 异步打开数据库
+        /// </summary>
+        /// <param name="DataSource">数据库地址</param>
+        /// <param name="UserId">用户名</param>
+        /// <param name="Paassword">密码</param>
+        /// <param name="DataBase">数据库名称【默认：master】</param>
+        public static async void OpenAsync(string DataSource, string UserId, string Paassword, string DataBase = "master")
+        {
+            sqlConnection = new SqlConnection(string.Format(connectionString, DataSource, DataBase, UserId, Paassword));
+            await sqlConnection.OpenAsync();
+        }
+        /// <summary>
+        /// 关闭数据库
+        /// </summary>
+        public static void Close()
+        {
+            if (sqlConnection != null && sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        /// <summary>
+        /// 获取所有数据库
+        /// </summary>
+        /// <returns></returns>
+        public static DataTable GetDataBase()
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlGetDataBases, sqlConnection);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+            return dt;
+        }
+        /// <summary>
+        /// 获取数据库中所有表
+        /// </summary>
+        /// <param name="DataBaseName">数据库名称</param>
+        /// <returns></returns>
+        public static DataTable GetTables(string DataBaseName)
+        {
+            sqlUseDataBase = $"USE {DataBaseName} ";
+            return GetDataTable(string.Format(sqlGetTables, DataBaseName));
+        }
+        public static DataTable GetTableInfo(string TableName)
+        {
+            sqlUesTable = TableName;
+            return GetDataTable(sqlUseDataBase + string.Format(sqlGetTableInfo, TableName));
+        }
+        /// <summary>
+        /// 执行SQL返回DataSet
+        /// </summary>
+        /// <param name="sqlQuery"></param>
+        /// <returns></returns>
+        public static DataSet GetDataSet(string sqlQuery)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlQuery, sqlConnection);
+                sqlDataAdapter.Fill(ds);
+            }
+            catch { }
+            return ds;
+        }
+        /// <summary>
+        /// 执行SQL返回DataTable
+        /// </summary>
+        /// <param name="sqlQuery"></param>
+        /// <returns></returns>
+        public static DataTable GetDataTable(string sqlQuery)
+        {
+            DataSet ds = GetDataSet(sqlQuery);
+            if (ds != null && ds.Tables.Count > 0)
+            {
+                return ds.Tables[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
